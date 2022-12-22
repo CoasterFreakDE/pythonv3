5ad175a49fd17705d940d86e92afbe82cfcde5e4
CoasterFreakDE <mleaked@gmail.com>
2022-12-22 09:18:54 +0100

    💚 Folge 6: LevelSystem

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import asyncio
 import logging
 import os
 from pathlib import Path
 
 import discord
+from aiomysql import Pool
+import aiofiles
 from discord.ext import commands
 
 from dotenv import load_dotenv
+
+from database import get_pool
 
 load_dotenv()
 
@@ -23,14 +28,36 @@ bot = commands.Bot(
 	delete_not_existing_commands=True
 )
 
+async def init_db():
+	pool: Pool = await get_pool()
+	async with aiofiles.open('database/structure.sql') as f:
+		sql = await f.read()
+
+	async with pool.acquire() as connection:
+		async with connection.cursor() as cursor:
+			for statement in sql.split(';'):
+				try:
+					await cursor.execute(statement)
+				except Exception as e:
+					log.warning(e)
+					continue
+	pool.close()
+	await pool.wait_closed()
+
+
 if __name__ == '__main__':
-	log.info('Starting bot...')
+	print('Starting bot...')
+
+	print('Loading database...')
+	asyncio.run(init_db())
+
+	print('Loading cogs...')
 	cogs = [file.stem for file in Path('cogs').glob('**/*.py') if not file.name.startswith('__')]
-	log.info(f'Loading {len(cogs)} cogs...')
+	print(f'Loading {len(cogs)} cogs...')
 
 	for cog in cogs:
 		bot.load_extension(f'cogs.{cog}')
-		log.info(f'Loaded cog {cog}')
+		print(f'Loaded cog {cog}')
 
 	token = os.getenv('BOT_TOKEN')
 	bot.run(token)
